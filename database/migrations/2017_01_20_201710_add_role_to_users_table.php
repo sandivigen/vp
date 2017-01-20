ca116543b9f6f3628181e04beec418f51ab97b67
sandivigen <sandivigen@gmail.com>
2017-01-20 20:30:20 +0300

    add migration user role

--- database/migrations/2017_01_20_201710_add_role_to_users_table.php
+++ database/migrations/2017_01_20_201710_add_role_to_users_table.php
@@ -13,7 +13,7 @@ class AddRoleToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->integer('role')->default(4)->after('avatar');
         });
     }
 
@@ -25,7 +25,7 @@ class AddRoleToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 }
